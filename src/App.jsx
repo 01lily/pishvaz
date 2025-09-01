ffa4954279db18e1c2de330dc620ff6029ae1057
01lily <38993060+01lily@users.noreply.github.com>
2025-09-01 11:12:43 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ProductGrid from "./components/ProductGrid";
+import ProductPage from "./components/ProductPage";
 
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ProductGrid status="available" />} />
+        <Route path="/comingsoon" element={<ProductGrid status="comingsoon" />} />
+        <Route path="/available" element={<ProductGrid status="available" />} />
+        <Route path="/sold" element={<ProductGrid status="sold" />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
+    </div>
+  );
+}
